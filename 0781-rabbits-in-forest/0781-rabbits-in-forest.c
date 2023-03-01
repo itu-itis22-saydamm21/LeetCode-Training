223d2460e751abf1a29a9055328c9a5b3644d0f8
Mustafa Saydam <98849333+itu-itis22-saydamm21@users.noreply.github.com>
2023-03-01 20:36:54 +0300

    Time: 7 ms (33.33%), Space: 5.9 MB (66.67%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.c
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.c
@@ -2,11 +2,11 @@ int numRabbits(int* answers, int answersSize) {
     int counts[1000] = {0}; // Assume no more than 1000 rabbits
     int minCount = 0;
     for (int i = 0; i < answersSize; i++) {
-        int ans = answers[i];
-        counts[ans]++;
-        if (counts[ans] == ans + 1) { // We've seen all rabbits of this color
-            minCount += ans + 1;
-            counts[ans] = 0; // Reset the count
+        int answer = answers[i];
+        counts[answer]++;
+        if (counts[answer] == answer + 1) { // We've seen all rabbits of this color
+            minCount += answer + 1;
+            counts[answer] = 0; // Reset the count
         }
     }
     // Add up the remaining rabbits that we haven't seen all of their color
