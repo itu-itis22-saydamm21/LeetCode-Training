6eecb61a8dcbb39c91b19d532db2f58bbf1f87a9
Mustafa Saydam <98849333+itu-itis22-saydamm21@users.noreply.github.com>
2023-09-16 00:54:13 +0300

    Time: 545 ms (55.65%), Space: 43 MB (76.25%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,36 +1,26 @@
+#define pii pair<int, int>
+
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        
-        unordered_map<int, vector<pair<int,int>>> adj;
-        int n = points.size();
-        for(int i = 0; i < n ; i++){
-            for(int j = i + 1; j < n ; j++){
-                int delta_x = abs(points[i][0] - points[j][0]); 
-                int delta_y = abs(points[i][1] - points[j][1]);
-                int cost = delta_x + delta_y;
-                adj[j].push_back({cost, i});
-                adj[i].push_back({cost, j});
-            }
-        }
-        
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<bool> visited(n, false);
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        int n =  points.size();
+        vector<int> vis(n+1, 0);
+        int ans = 0;
         pq.push({0, 0});
-        int sum = 0;
         while(!pq.empty()){
-            int cost = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            if(visited[node])
+            auto node = pq.top(); pq.pop();
+            if(vis[node.second])
                 continue;
-            visited[node] = true;
-            sum += cost;
-            for(auto a : adj[node]){
-                if(!visited[a.second])
-                    pq.push(a);
+            vis[node.second]++;
+            ans+=node.first;
+            vector<int> co = points[node.second];
+
+            for(int i=1; i<n; i++){
+                if(!vis[i])
+                    pq.push({abs(co[0] - points[i][0]) + abs(co[1] - points[i][1]), i} );
             }
         }
-        return sum;
+        return ans;
     }
 };
