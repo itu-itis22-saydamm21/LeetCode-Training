7f6057cb0fa5b097c6fc28669d3255fea89410cb
Mustafa Saydam <98849333+itu-itis22-saydamm21@users.noreply.github.com>
2024-06-23 01:17:46 +0300

    Time: 92 ms (35.85%), Space: 47.1 MB (28.28%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector< vector<pair<int,int>> > adj(n + 1);
+        for(auto time : times){
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
+            adj[u].push_back({v, w});
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
         vector<int> dist(n + 1, INT_MAX);
+        
+        pq.push({0, k}); // {distance, starting_node}
         dist[k] = 0;
         
-        for(int i = 0; i < n - 1; i++){
-            for(auto time : times){
-                int u = time[0];
-                int v = time[1];
-                int w = time[2];
-                if(dist[u] != INT_MAX && dist[u] + w < dist[v]){
+        while(!pq.empty()){
+            int currentDist = pq.top().first; // To sort by distance 
+            int u = pq.top().second;
+            pq.pop();
+            
+            if(dist[u] < currentDist) continue;
+            
+            for(auto adj : adj[u]){
+                int v = adj.first;
+                int w = adj.second;
+                if(dist[u] + w < dist[v]){
                     dist[v] = dist[u] + w;
+                    pq.push({dist[v], v});
                 }
             }
         }
         int maxDist = 0;
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i <= n ; i++){
             if(dist[i] == INT_MAX) return -1;
             maxDist = max(maxDist, dist[i]);
         }
