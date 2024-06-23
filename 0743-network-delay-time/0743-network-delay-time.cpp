fd94b85cd657bee4ead1bd409f153ba3947fb519
Mustafa Saydam <98849333+itu-itis22-saydamm21@users.noreply.github.com>
2024-06-23 03:01:44 +0300

    Time: 106 ms (14.87%), Space: 44.4 MB (59.53%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector< vector<pair<int,int>> > adj(n + 1);
+        vector< vector<int> > dist(n + 1, vector<int>(n + 1, INT_MAX));
+        
+        for(int i = 1; i <= n; i++){
+            dist[i][i] = 0;
+        }
+        
         for(auto time : times){
             int u = time[0];
             int v = time[1];
             int w = time[2];
-            adj[u].push_back({v, w});
+            dist[u][v] = w;
         }
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
-        vector<int> dist(n + 1, INT_MAX);
         
-        pq.push({0, k}); // {distance, starting_node}
-        dist[k] = 0;
-        
-        while(!pq.empty()){
-            int currentDist = pq.top().first; // To sort by distance 
-            int u = pq.top().second;
-            pq.pop();
-            
-            if(dist[u] < currentDist) continue;
-            
-            for(auto adj : adj[u]){
-                int v = adj.first;
-                int w = adj.second;
-                if(dist[u] + w < dist[v]){
-                    dist[v] = dist[u] + w;
-                    pq.push({dist[v], v});
+        for(int via = 1; via <= n; via++){
+            for(int u = 1; u <= n; u++){
+                for(int v = 1; v <= n; v++){
+                    if (dist[u][via] != INT_MAX && dist[via][v] != INT_MAX
+                       && dist[u][via] + dist[via][v] < dist[u][v]){
+                        dist[u][v] = dist[u][via] + dist[via][v];
+                    }
                 }
             }
         }
+        
         int maxDist = 0;
-        for(int i = 1; i <= n ; i++){
-            if(dist[i] == INT_MAX) return -1;
-            maxDist = max(maxDist, dist[i]);
+        for(int i = 1; i <= n; i++){
+            if(dist[k][i] == INT_MAX) return -1;
+            maxDist = max(maxDist, dist[k][i]);
         }
         return maxDist;
     }
